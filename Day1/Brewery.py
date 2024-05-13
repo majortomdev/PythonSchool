1b97bd58dc015ba9efcfb9133c1d47f2d35efcd0
Joseph Kinahan <kinahanj25@gmail.com>
2024-05-13 17:30:14 +0100

    Day1_python_intro

---
+++ Day1/Brewery.py
@@ -0,0 +1,3 @@
+print("The new name for your beer brand is\n '"+input("Please enter a beast you would "
+  "be afraid of: ")+"'s "+input("Please enter a luscious type of cake: ")+"'\nCONGRATULATIONS")
+
---
+++ Day1/First_project.py
@@ -0,0 +1,2 @@
+print("The new name for your beer brand is\n '"+input("Please enter a beast you would be afraid of ")+"'s "+input("Please enter a luscious type of cake ")+"'")
+
